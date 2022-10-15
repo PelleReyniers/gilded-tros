ea983b55f9675b94388538c4aa95394273450455
Pelle Reyniers <pellereyniers@gmail.com>
2022-10-15 16:22:45 +0200

    created dataset generation method. to be used later in unit tests ass wel as main method.

--- Java/src/test/java/com/gildedtros/TexttestFixture.java
+++ Java/src/test/java/com/gildedtros/TexttestFixture.java
@@ -4,19 +4,7 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("AXXES CODE KATA - GILDED TROS");
 
-        Item[] items = new Item[] {
-                new Item("Ring of Cleansening Code", 10, 20),
-                new Item("Good Wine", 2, 0),
-                new Item("Elixir of the SOLID", 5, 7),
-                new Item("B-DAWG Keychain", 0, 80),
-                new Item("B-DAWG Keychain", -1, 80),
-                new Item("Backstage passes for Re:Factor", 15, 20),
-                new Item("Backstage passes for Re:Factor", 10, 49),
-                new Item("Backstage passes for HAXX", 5, 49),
-                // these smelly items do not work properly yet
-                new Item("Duplicate Code", 3, 6),
-                new Item("Long Methods", 3, 6),
-                new Item("Ugly Variable Names", 3, 6) };
+        Item[] items = generateDataSet();
 
         GildedTros app = new GildedTros(items);
 
@@ -34,6 +22,22 @@ public class TexttestFixture {
             System.out.println();
             app.updateQuality();
         }
+    }
+
+    public static Item[] generateDataSet(){
+        return new Item[] {
+                new Item("Ring of Cleansening Code", 10, 20),
+                new Item("Good Wine", 2, 0),
+                new Item("Elixir of the SOLID", 5, 7),
+                new Item("B-DAWG Keychain", 0, 80),
+                new Item("B-DAWG Keychain", -1, 80),
+                new Item("Backstage passes for Re:Factor", 15, 20),
+                new Item("Backstage passes for Re:Factor", 10, 49),
+                new Item("Backstage passes for HAXX", 5, 49),
+                // these smelly items do not work properly yet
+                new Item("Duplicate Code", 3, 6),
+                new Item("Long Methods", 3, 6),
+                new Item("Ugly Variable Names", 3, 6) };
     }
 
 }
